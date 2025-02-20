b6ba8aca176a2e0e120eb1722c3437a7414bb2d4
Braga <BragaPensador@gmail.com>
2025-02-19 23:23:00 -0300

    Estilização dos elementos

--- lib/main.dart
+++ lib/main.dart
@@ -54,17 +54,34 @@ class MyHomePage extends StatelessWidget {
                   ), //Espaçamentos das margens do "value"
 
                   decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black, width: 2),
+                    border: Border.all(color: Colors.purple, width: 2),
                   ), //Borda do "value"
 
                   padding: EdgeInsets.all(10),
-                  child: Text(tr.value.toString()),
-                ), 
-
+                  child: Text(
+                    tr.value.toString(),
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple,
+                    ),
+                  ),
+                ),
                 Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(tr.title),
-                    Text(tr.date.toString()),
+                    Text(
+                      tr.title,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(tr.date.toString(),
+                        style: const TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey
+                        )),
                   ],
                 )
               ],
