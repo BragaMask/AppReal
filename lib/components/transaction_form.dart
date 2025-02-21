f1de7046738e4455301660368b4dcbfc4b7ddea8
Braga <BragaPensador@gmail.com>
2025-02-21 14:53:10 -0300

    adição do teclado número no TextField

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -10,6 +10,16 @@ class TransactionForm extends StatelessWidget {
 
   TransactionForm(this.onsubmit);
 
+  _submitForm() {
+    final title = titleController.text;
+    final value = double.tryParse(valueController.text) ?? 0.0;
+
+    if (title.isEmpty || value <= 0) {
+      return;
+    }
+    onsubmit(title, value);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -20,12 +30,15 @@ class TransactionForm extends StatelessWidget {
           children: [
             TextField(
               controller: titleController,
+              onSubmitted: (_) => _submitForm(),
               decoration: const InputDecoration(
                 labelText: 'título',
               ),
             ),
             TextField(
               controller: valueController,
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              onSubmitted: (_) => _submitForm(),
               decoration: InputDecoration(
                 labelText: 'valor (R\$)',
               ),
